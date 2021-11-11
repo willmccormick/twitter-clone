9845f1c74e131cd974bd5796c4e818dc6a679649
William McCormick <wmccormick1000@gmail.com>
2021-11-10 23:35:01 -0800

    Update query.php
    Encapsulates the query into a php function.

--- query.php
+++ query.php
@@ -1,47 +1,68 @@
 <?php
 
+function tableFromQuery($query){
+    
+    //put your seattle u name here
+    $seattleu = "wmccormick";
+    
+    //used to access database
+    $servername = "cssql.seattleu.edu";
+    $username = "ll_" . $seattleu;
+    $password = "ll_" . $seattleu;
+    $dbname = "ll_" . $seattleu;
+    
+    //return value
+    $html;
+
+    //create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    //quit if connection fails
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    //states the query at the top of the page
+    $html = $html . "<h1>QUERY: " . $query . "</h1><br>";
+
+    //gets the result of a query
+    $result = mysqli_query($conn, $query);
+
+    if (mysqli_num_rows($result) > 0) {
+        
+        //starts the table
+        $html = $html . "<table>";
+        
+        //creates a row in the table for the headers
+        $html = $html . "<tr>";
+        while ($field = mysqli_fetch_field($result)) {
+            $html = $html . "<th>" . $field->name . "</th>";
+        }
+        $html = $html . "</tr>";
+        
+        //creates the remaining rows in the table
+        while($row = mysqli_fetch_row($result)) {
+            $html = $html . "<tr>";
+            for($i = 0; $i < mysqli_num_fields($result); $i++) {
+                $html = $html . "<td>" . $row[$i] . "</td>";  
+            }
+            $html = $html . "</tr>";
+        }
+        
+        $html = $html . "</table>";
+    } else {
+        $html = $html . "0 results";
+    }
+    
+    // Free result set and close connection
+    mysqli_free_result($result);
+    mysqli_close($conn);
+    
+    return $html;
+}
+
 echo "<link rel='stylesheet' href='stylesheet.css'>";
 echo "<link rel='stylesheet' href='query_stylesheet.css'>";
 
-$servername = "cssql.seattleu.edu";
-$username = "ll_wmccormick";
-$password = "ll_wmccormick";
-$dbname = "ll_wmccormick";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-$sql = $_POST["query"];
-
-echo "<h1>QUERY: " . $_POST["query"] . "</h1><br>";
-
-$result = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($result) > 0) {
-    echo "<table><tr>";
-    while ($field = mysqli_fetch_field($result)) {
-        echo "<th>" . $field->name . "</th>";
-    }
-    echo "</tr>";
-    while($row = mysqli_fetch_row($result)) {
-        echo "<tr>";
-        for($i = 0; $i < mysqli_num_fields($result); $i++) {
-            echo "<td>" . $row[$i] . "</td>";  
-        }
-        echo "</tr>";
-    }
-    echo "</table>";
-} else {
-    echo "0 results";
-}
-
-// Free result set
-mysqli_free_result($result);
-mysqli_close($conn);
-
+echo tableFromQuery($_POST["query"]);
 ?> 
